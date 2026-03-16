fc5f0b8d1afdd1f2165ac13c42ce206942152f00
davidliu <davidliu@deviange.net>
2026-03-16 23:13:35 +0900

    ios: add enableMultitaskingCameraAccess flag

--- ios/LiveKitExpoAppDelegate.swift
+++ ios/LiveKitExpoAppDelegate.swift
@@ -1,9 +1,14 @@
 import ExpoModulesCore
 import livekit_react_native
+import livekit_react_native_webrtc
 
 public class LiveKitExpoAppDelegate: ExpoAppDelegateSubscriber {
   public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
     LivekitReactNative.setup()
+    if let enableMultitasking = Bundle.main.object(forInfoDictionaryKey: "io.livekit.reactnative.expo.ENABLE_MULTITASKING_CAMERA_ACCESS") as? Bool, enableMultitasking {
+      WebRTCModuleOptions.sharedInstance().enableMultitaskingCameraAccess = true
+    }
+
     return true
   }
 }
